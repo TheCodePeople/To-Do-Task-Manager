1bbf47ea2712b9f930a082ef927c2071c7ec3cb7
husseinshaheed <shaheey33@gmail.com>
2023-08-01 18:54:22 +0300

    code addButton to ToDoList

---
+++ app.js
@@ -0,0 +1,28 @@
+const theMainUl = document.getElementsByClassName("theMainUl");
+const theSubmitButton = document.getElementById("submit");
+const mainInput = document.getElementById("mainInput");
+theSubmitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const newDivForUl = document.createElement("div");
+  const newDivForLi = document.createElement("div");
+  const newListItem = document.createElement("li");
+  const doneButton = document.createElement("button");
+  const removeButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  newDivForUl.classList.add("newDivForUl");
+  newDivForLi.classList.add("newDivForLi");
+  doneButton.classList.add("doneButtonAndRemoveButton");
+  removeButton.classList.add("doneButtonAndRemoveButton");
+  editButton.classList.add("doneButtonAndRemoveButton");
+  newListItem.textContent = mainInput.value;
+  removeButton.textContent = "REMOVE";
+  doneButton.textContent = "DONE";
+  editButton.textContent = "EDIT";
+  theMainUl[0].appendChild(newDivForUl);
+  newDivForUl.appendChild(newDivForLi);
+  newDivForUl.appendChild(newListItem);
+  newDivForLi.appendChild(doneButton);
+  newDivForLi.appendChild(editButton);
+  newDivForLi.appendChild(removeButton);
+});
+
