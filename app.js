01cdef452dbf12913684e797483ec321d1284d1f
husseinshaheed <shaheey33@gmail.com>
2023-08-05 03:21:35 +0300

    make edit and donnig button and complite all todo project

--- app.js
+++ app.js
@@ -1,39 +1,73 @@
 const theMainUl = document.getElementsByClassName("theMainUl");
 const theSubmitButton = document.getElementById("submit");
 const mainInput = document.getElementById("mainInput");
+const container2 = document.getElementsByClassName("container2");
+const continuer = document.getElementsByClassName("continuer");
+
 theSubmitButton.addEventListener("click", (event) => {
+  
   event.preventDefault();
-  const newDivForUl = document.createElement("div");
-  const newDivForLi = document.createElement("div");
-  const newListItem = document.createElement("li");
-  const doneButton = document.createElement("button");
+  const newDivForAdd = document.createElement("div");
+  const newListItem = document.createElement("input");
   const removeButton = document.createElement("button");
   const editButton = document.createElement("button");
-  newDivForUl.classList.add("newDivForUl");
-  newDivForLi.classList.add("newDivForLi");
-  doneButton.classList.add("doneButtonAndRemoveButton");
+  editButton.title="EDIT" 
+  removeButton.title="REMOVE" 
+
+
+  newDivForAdd.classList.add("newDivForAdd");
+  // newDivForLi.classList.add("newDivForLi");
+  newListItem.classList.add("newListItem");
+  newListItem.setAttribute("readonly", "readonly");
   removeButton.classList.add("doneButtonAndRemoveButton");
   editButton.classList.add("doneButtonAndRemoveButton");
-  if (mainInput.value === "") {
-    alert("لازم  تخلي كلام")
-  }else {
-    newListItem.textContent = mainInput.value;
+  if (!mainInput.value.trim()) {
+    alert("لازم  تخلي كلام");
+  } else {
+    newListItem.value = mainInput.value;
     mainInput.value = "";
-    removeButton.textContent = "REMOVE";
-    doneButton.textContent = "DONE";
-    editButton.textContent = "EDIT";
-    theMainUl[0].appendChild(newDivForUl);
-    newDivForUl.appendChild(newDivForLi);
-    newDivForUl.appendChild(newListItem);
-    newDivForLi.appendChild(doneButton);
-    newDivForLi.appendChild(editButton);
-    newDivForLi.appendChild(removeButton);
+    removeButton.innerHTML = "&#10060;";
+    editButton.innerHTML = "&#9997;";
+    editButton.style.fontSize = "20px";
+    removeButton.style.fontSize = "20px";
+    container2[0].appendChild(newDivForAdd);
+    newDivForAdd.appendChild(newListItem);
+    newDivForAdd.appendChild(editButton);
+    newDivForAdd.appendChild(removeButton);
   }
- 
   removeButton.addEventListener("click", () => {
-    newDivForUl.remove();
+    newDivForAdd.remove();
   });
   editButton.addEventListener("click", () => {
-    
+    if (editButton.innerHTML === "✍") {
+      editButton.innerHTML = "&#9989;";
+      editButton.title="SAVE" 
+      editButton.style.fontSize = "20px";
+      newListItem.removeAttribute("readonly");
+      newListItem.focus();
+    } else {
+      editButton.innerHTML = "&#9997;";
+      editButton.style.fontSize = "20px";
+      newListItem.setAttribute("readonly", "readonly");
+    }
+  });
+  newListItem.addEventListener("click", (e) => {
+    e.target.style.background = "yellow";
+    editButton.remove();
+    removeButton.remove();
+    const unCheck = document.createElement("button");
+    unCheck.classList.add("doneButtonAndRemoveButton");
+    unCheck.innerHTML = "&#10008;";
+    unCheck.style.fontSize = "20px";
+    unCheck.title="UNCHECK" 
+    newDivForAdd.appendChild(unCheck);
+    unCheck.addEventListener("click", () => {
+      e.target.style.background = "white";
+      editButton.style.fontSize = "20px";
+      editButton.innerHTML = "&#9997;";
+      newDivForAdd.appendChild(editButton);
+      newDivForAdd.appendChild(removeButton);
+      unCheck.remove();
+    });
   });
 });
